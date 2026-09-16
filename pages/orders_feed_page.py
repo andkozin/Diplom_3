6d3579c2e587a60eeef5355b1fc83f282bbc2634
Andrey Kozin <andkozin@gmail.com>
2026-09-16 17:35:38 +0300

    driver.get на super().open()

--- pages/orders_feed_page.py
+++ pages/orders_feed_page.py
@@ -1,10 +1,9 @@
 # pages/orders_feed_page.py
-import allure
 
+import allure
 from pages.base_page import BasePage
 from locators import ModalLocators as ML
 from locators import FeedLocators as FL
-
 import urls
 import re
 
@@ -13,9 +12,8 @@ class OrdersFeedPage(BasePage):
 
     @allure.step("Открываем Ленту заказов")
     def open(self):
-        self.driver.get(urls.FEED_URL)
-
-    
+        
+        super().open(urls.FEED_URL)
 
     @allure.step("Получаем значение счетчика Выполнено за все время")
     def get_total_counter(self):
@@ -23,17 +21,15 @@ class OrdersFeedPage(BasePage):
         digits = re.findall(r'\d+', text)
         return int(digits[0]) if digits else 0
 
-    @allure.step("Получаем значение счетчика  Выполнено за сегодня")
+    @allure.story("Получаем значение счетчика Выполнено за сегодня")
     def get_today_counter(self):
         text = self.get_text_content(FL.COUNTER_TODAY)
         digits = re.findall(r'\d+', text)
         return int(digits[0]) if digits else 0
 
-
     @allure.step("Получаем номера заказов в статусе В работе")
     def get_in_progress_orders(self):
         self.wait_elements_with_digits(FL.IN_PROGRESS_ITEMS)
-
         items = self.find_elements(FL.IN_PROGRESS_ITEMS)
         result = []
         for el in items:
@@ -42,7 +38,6 @@ class OrdersFeedPage(BasePage):
             if digits:
                 result.append(digits.lstrip('0'))
         return result
-
 
     @allure.step("Ждем появления реального номера заказа (не 9999)")
     def wait_real_order_number(self):
@@ -60,15 +55,13 @@ class OrdersFeedPage(BasePage):
         self.click(ML.ORDER_MODAL_CLOSE)
         self.wait_invisible(ML.ORDER_MODAL)
 
-
-    @allure.step("Ждем увеличения счетчика за все время ")
+    @allure.step("Ждем увеличения счетчика за все время")
     def wait_total_counter_increases(self, expected_value):
-        
         self.wait.until(lambda d: self.get_total_counter() > expected_value)
         return self.get_total_counter()
 
     @allure.step("Ждем увеличения счетчика Выполнено за сегодня")
     def wait_today_counter_increases(self, expected_value):
-       
         self.wait.until(lambda d: self.get_today_counter() > expected_value)
         return self.get_today_counter()
+
